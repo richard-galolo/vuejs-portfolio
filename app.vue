39469caed183a04d0ef53b1923e4fbb6b262fbe6
Richard Galolo <galolorichard34@gmail.com>
2024-12-18 14:12:37 +0800

    added vercel speed insights

--- app.vue
+++ app.vue
@@ -1,7 +1,12 @@
+<script setup>
+  import { SpeedInsights } from "@vercel/speed-insights/nuxt"
+</script>
+
 <template>
   <div>
     <NuxtLayout>
       <NuxtPage />
     </NuxtLayout>
   </div>
+  <SpeedInsights />
 </template>
