39afcca994b42e332a3d41363ba46d3b3cf4236c
Richard Galolo <galolorichard34@gmail.com>
2024-12-23 20:54:45 +0800

    added share icon

--- pages/blogs/ticket-flow.vue
+++ pages/blogs/ticket-flow.vue
@@ -46,11 +46,10 @@
 
 <template>
   <div class="relative mx-auto max-w-xl min-h-screen flex flex-col px-4 text-sm">
-    <!-- Main Content -->
     <main class="flex-grow">
-      <!-- Navbar Component -->
       <Navbar />
 
+      <!-- Breadcrumbs -->
       <div class="breadcrumbs text-sm">
         <ul class="text-[#6c757d]">
           <li>
@@ -63,52 +62,61 @@
         </ul>
       </div>
 
-      <!-- Introduction -->
+      <!-- Title -->
       <section class="mt-4">
-        <h1 class="text-2xl font-semibold">Mastering Ticket Flow in Project Development: Boosting Efficiency and Transparency</h1>
-        <p class="font-light text-sm text-[#6c757d] mt-2">December 23, 2024</p>
+        <h1 class="text-2xl font-semibold">Simplifying Ticket Flow for Efficient Project Development</h1>
+        <div class="flex justify-between">
+          <div class="font-light text-sm text-[#6c757d] mt-2">December 23, 2024</div>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="18"
+            height="18"
+            fill="none"
+            viewBox="0 0 24 24"
+            class="mt-2">
+            <path fill="currentColor" fill-rule="evenodd" d="M15.218 4.931a.4.4 0 0 1-.118.132l.012.006a.45.45 0 0 1-.292.074.5.5 0 0 1-.3-.13l-2.02-2.02v7.07c0 .28-.23.5-.5.5s-.5-.22-.5-.5v-7.04l-2 2a.45.45 0 0 1-.57.04h-.02a.4.4 0 0 1-.16-.3.4.4 0 0 1 .1-.32l2.8-2.8a.5.5 0 0 1 .7 0l2.8 2.79a.42.42 0 0 1 .068.498m-.106.138.008.004v-.01zM16 7.063h1.5a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-11c-1.1 0-2-.9-2-2v-10a2 2 0 0 1 2-2H8a.5.5 0 0 1 .35.15.5.5 0 0 1 .15.35.5.5 0 0 1-.15.35.5.5 0 0 1-.35.15H6.4c-.5 0-.9.4-.9.9v10.2a.9.9 0 0 0 .9.9h11.2c.5 0 .9-.4.9-.9v-10.2c0-.5-.4-.9-.9-.9H16a.5.5 0 0 1 0-1" clip-rule="evenodd"></path>
+          </svg>
+        </div>
 
-        <div class="card bg-base-100 w-100 mt-6">
-          <a href="/_nuxt/assets/img/ticket-flow-diagram.png"
+        <!-- Image -->
+        <div class="mt-6">
+          <a
+            href="/img/ticket-flow-diagram.png"
             target="_blank"
             rel="noopener noreferrer"
-            aria-label="View source on GitHub">
+            aria-label="View image">
             <figure>
             <img
-                src="~/assets/img/ticket-flow-diagram.png"
-                alt="Ticket Flow Diagram" />
+              src="/img/ticket-flow-diagram.png"
+              lazy="loading"
+              alt="Ticket Flow Diagram" />
             </figure>
           </a>
         </div>
 
-        <p class="leading-6 mt-6">Effective project management begins with a structured ticket flow. This process helps keep development on track, reduces bottlenecks, and ensures your team delivers high-quality products on time.</p>
-        <p class="leading-6 mt-4">In this post, we’ll walk you through each stage of the ticket flow and explain how to leverage it to optimize your development workflow.</p>
+        <h2 class="text-lg font-semibold mt-6">Introduction</h2>
+        <p class="leading-6 mt-2">Effective project management begins with a structured ticket flow. This process helps keep development on track, reduces bottlenecks, and ensures your team delivers high-quality products on time.</p>
+
+        <p class="leading-6 mt-2">In this post, we’ll walk you through each stage of the ticket flow and explain how to leverage it to optimize your development workflow.</p>
       </section>
 
-      <!-- Table of Contents -->
-      <section class="mt-6">
-        <h2 class="text-lg font-semibold">Table of Contents</h2>
-        <ul class="leading-6">
+      <h3 class="text-lg font-semibold mt-6">Stages of Ticket Flow</h3>
+
+      <section>
+        <ul>
           <li
             v-for="(stage, index) in stages"
-            :key="stage.id">
-            <a :href="`#${stage.id}`">{{ index + 1 }}. {{ stage.title.split(":")[0] }}</a>
+            :key="stage.id"
+            :class="index != 0 ? 'mt-6' : 'mt-4' "
+            :id="stage.id">
+            <p class="font-semibold">{{ index + 1 }}. {{ stage.title }}</p>
+            <p class="leading-6 mt-1">{{ stage.content }}</p>
           </li>
         </ul>
       </section>
 
-      <!-- Content -->
-      <section
-        v-for="(stage, index) in stages"
-        :key="stage.id"
-        class="mt-10"
-        :id="stage.id">
-        <h2 class="text-lg font-semibold">{{ index + 1 }}. {{ stage.title }}</h2>
-        <p class="leading-6 mt-1">{{ stage.content }}</p>
-      </section>
-
       <section class="mt-6">
-        <h2 class="text-lg font-semibold">Conclusion</h2>
+        <h4 class="text-lg font-semibold">Conclusion</h4>
         <p class="leading-6">Mastering ticket flow isn’t just about moving tasks from one stage to the next. It’s about creating a system that enhances efficiency, fosters collaboration, and ensures transparency. With a solid ticket flow, your team will be better equipped to deliver high-quality software on time, while keeping everyone aligned with project goals.</p>
       </section>
     </main>
